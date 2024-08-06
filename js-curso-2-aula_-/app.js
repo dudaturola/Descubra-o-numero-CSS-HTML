249c35029ec607692b3c3493673be2a4d0dfd7dc
Maria Eduarda <idturola@gmail.com>
2024-08-06 09:10:06 -0300

    Update app.js

--- js-curso-2-aula_-/app.js
+++ js-curso-2-aula_-/app.js
@@ -1,9 +1,22 @@
 let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
+titulo.innerHTML = 'Hora do desafio';
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+function exibirMensagemNoConsole() {
+    console.log('O botão foi clicado');
+};
 
-function verificarChute() {
-    console.log('O botão foi clicado')
+function exibirAlerta() {
+    alert('Eu amo JS');
+}
+
+function exibirPrompt() {
+    let nomeDaCidade = prompt('Digite o nome de uma cidade do Brasil que você gosta muito:')
+    alert(`Estive em ${nomeDaCidade} e lembrei de você`)
+}
+
+function somarValores() {
+    let primeiroNumero = parseInt(prompt('Digite o primeiro número'));
+    let segundoNumero = parseInt(prompt('Digite o segundo número'));
+    let resultado = primeiroNumero + segundoNumero;
+    alert(`${primeiroNumero} + ${segundoNumero} = ${resultado}`)
 }
