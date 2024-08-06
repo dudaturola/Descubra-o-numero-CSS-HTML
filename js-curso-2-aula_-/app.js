20f893a4be1c44170cb392f58ea5246b51da93c8
Maria Eduarda <idturola@gmail.com>
2024-08-06 09:28:36 -0300

    Update app.js

--- js-curso-2-aula_-/app.js
+++ js-curso-2-aula_-/app.js
@@ -9,14 +9,22 @@ function exibirAlerta() {
     alert('Eu amo JS');
 }
 
-function exibirPrompt() {
-    let nomeDaCidade = prompt('Digite o nome de uma cidade do Brasil que você gosta muito:')
-    alert(`Estive em ${nomeDaCidade} e lembrei de você`)
+// function exibirPrompt() {
+//     let nomeDaCidade = prompt('Digite o nome de uma cidade do Brasil que você gosta muito:')
+//     alert(`Estive em ${nomeDaCidade} e lembrei de você`)
+// }
+
+// function somarValores() {
+//     let primeiroNumero = parseInt(prompt('Digite o primeiro número'));
+//     let segundoNumero = parseInt(prompt('Digite o segundo número'));
+//     let resultado = primeiroNumero + segundoNumero;
+//     alert(`${primeiroNumero} + ${segundoNumero} = ${resultado}`)
+// }
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
 }
 
-function somarValores() {
-    let primeiroNumero = parseInt(prompt('Digite o primeiro número'));
-    let segundoNumero = parseInt(prompt('Digite o segundo número'));
-    let resultado = primeiroNumero + segundoNumero;
-    alert(`${primeiroNumero} + ${segundoNumero} = ${resultado}`)
-}
+exibirTextoNaTela('h1','Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
